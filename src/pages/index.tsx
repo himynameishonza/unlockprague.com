ef65a2d2b24fc9075c3a58eee54561236418f11a
Jan Rádl <radl.honza@gmail.com>
2023-02-08 08:46:35 +0100

    Meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <title>Unlock - Claim goes here</title>
         <meta
           name="description"
-          content="At DeceptiConf you’ll learn about the latest dark patterns being developed to trick even the smartest visitors, and you’ll learn how to deploy them without ever being detected."
+          content="Unlock is a civic tech summit showcasing cutting-edge campaigns and featuring expert masterclasses that'll reboot your mindset and inspire you to upgrade your civic projects."
         />
       </Head>
     </>
